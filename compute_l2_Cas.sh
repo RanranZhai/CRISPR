fce4141a06518a35aa927c44a14671096e99e238
Ranran Zhai <1737189953@qq.com>
2022-04-11 12:31:09 +0800

    Add files via upload

--- compute_l2_Cas.sh
+++ compute_l2_Cas.sh
@@ -8,8 +8,8 @@ for i in $files;do
 	var=${i##1000G.EUR.QC.}
 	var=${var%%.bim}
 	bim=/home/ranran/ldsc/1000G_EUR_Phase3_plink/1000G.EUR.QC.$var
-	annot=/opt/working/projects/prj_026_CRISPR/Cas/$1/annotation/$var.annot.gz ## prefix of annot.gz file, without chromosome number, which was refer to $var
-	out=/opt/working/projects/prj_026_CRISPR/Cas/$1/annotation/$var
+	annot=/opt/working/projects/prj_026_CRISPR/Rev2203/Cas/$1/annotation/$var.annot.gz ## prefix of annot.gz file, without chromosome number, which was refer to $var
+	out=/opt/working/projects/prj_026_CRISPR/Rev2203/Cas/$1/annotation/$var
 	snp=/home/ranran/ldsc/listHM3.txt
 	echo $var
 	echo $bim
--- make_annot_gzip_Cas.sh
+++ make_annot_gzip_Cas.sh
@@ -9,14 +9,14 @@ files=$(ls *.bim)
 
 echo $files
 
-mkdir /opt/working/projects/prj_026_CRISPR/Cas/$1/annotation
+mkdir /opt/working/projects/prj_026_CRISPR/Rev2203/Cas/$1/annotation
 
 for i in $files;do
 	var=${i##1000G.EUR.QC.}
 	var=${var%%.bim}
 	bim=/home/ranran/ldsc/1000G_EUR_Phase3_plink/1000G.EUR.QC.$var.bim
-	annot=/opt/working/projects/prj_026_CRISPR/Cas/$1/annotation/$var.annot
-	bed=/opt/working/projects/prj_026_CRISPR/Cas/$1/Top10.bed
+	annot=/opt/working/projects/prj_026_CRISPR/Rev2203/Cas/$1/annotation/$var.annot
+	bed=/opt/working/projects/prj_026_CRISPR/Rev2203/Cas/$1/Top10.bed
 	echo $var
 	echo $bim
 	echo $annot
@@ -28,7 +28,7 @@ done
 ### second argument : explaintory (e.g. TTTA, TTTG or TTTC)
 
 
-gzip -r /opt/working/projects/prj_026_CRISPR/Cas/$1/annotation/*.annot
+gzip -r /opt/working/projects/prj_026_CRISPR/Rev2203/Cas/$1/annotation/*.annot
 
 
 
